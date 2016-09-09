06c4ff77d3c1a9ef612ba7c365b474099a443668
Hyunseung Do <gustmd1210@gmail.com>
2016-09-09 18:29:46 -0400

    figured out better alternative. had to fix retval *= temp to retval *= base from professor's page.

--- Lab1.java
+++ Lab1.java
@@ -1,21 +1,77 @@
 //alternative lab instructor: tug13683
 public class Lab1{
     public static void main(String args[]){
-        
+        /*
+        for (int i = 1; i <= 100; i++){
+            MyPow.countGbl = 0;
+            double product = raise(2.0, i);
+            System.out.println("Product (raise): " + product);
+            System.out.println("Count: " + MyPow.countGbl);
+        }
+        */
+        System.out.println(raise2(2.0, 10)); 
     }
 
+    //not the best solution
+    
     public static double raise(double base, int exp){
-        if (base == 0.0 && exp < 0){ //zero to negative power returns infinity
+        //zero to negative power returns infinity
+        if (base == 0.0 && exp < 0){ 
             return Double.POSITIVE_INFINITY;
         }
-        else if (base == 0.0 && exp >= 0){ //zero to whole numbers returns zero
+        //zero to whole numbers returns zero
+        else if (base == 0.0 && exp >= 0){ 
             return 0.0;
         }
-        else if (exp == 1){ //anything raised to power of 1 returns base
+        //anything raised to power of 1 returns base
+        else if (exp == 1){ 
             return base;
         }
-        else { //actual exponential calculation
-            return base + exp;
+        
+        //fixing negative exponents
+        if (base != 0.0 && exp < 0){
+            base = 1.0 / base;
+            exp = -exp;
         }
+
+        //even
+        if (exp % 2 == 0)
+            return raise(base, exp/2) * raise(base, exp/2);
+        //odd
+        else
+            return raise(base, exp/2) * raise(base, exp/2) * base;
     }
+    
+    public static double raise2(double base, int exp){
+        double temp = 0.0;
+        double retval = 0.0;
+        //zero to negative power returns infinity
+        if (base == 0.0 && exp < 0){ 
+            return Double.POSITIVE_INFINITY;
+        }
+        //zero to whole numbers returns zero
+        else if (base == 0.0 && exp >= 0){ 
+            return 0.0;
+        }
+        //anything raised to power of 1 returns base
+        else if (exp == 1){ 
+            return base;
+        }
+        
+        //fixing negative exponents
+        if (base != 0.0 && exp < 0){
+            base = 1.0 / base;
+            exp = -exp;
+        }
+        temp = raise2(base, exp / 2);
+        retval = temp * temp; //count this multiply as explained later
+        if (exp % 2 == 1){
+            retval *= base; //count this multiply as explained later
+        }
+        return retval;
+    }
+}
+
+class MyPow{
+    public static int countGbl = 0;
 }
